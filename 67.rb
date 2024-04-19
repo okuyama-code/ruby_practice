d941fcaf13897e33a29ee3651faed5e858348baf
okuyama-code <ooyy0121@gmail.com>
2024-04-19 14:09:34 +0900

    ６８

---
+++ 67.rb
@@ -0,0 +1,19 @@
+
+
+# def find_ABC_position(n, s)
+#   (0..n-3).each do |i|
+#     if s[i..i+2] == "ABC"
+#       return i + 1 # 1-indexed position
+#     end
+#   end
+#   return -1
+# end
+
+# # 入力を受け取る
+# n = gets.to_i
+# s = gets.chomp
+
+# # 結果を出力する
+# puts find_ABC_position(n, s)
+
+puts "abcabcabc".size
